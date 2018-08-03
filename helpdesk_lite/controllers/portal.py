f368d4611e53afdb3c5af1e3c8ddcc8f5ae22806
mustafirus <golubev@nt.net.ua>
2018-08-03 12:29:12 +0300

    first release

--- helpdesk_lite/controllers/portal.py
+++ helpdesk_lite/controllers/portal.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 from odoo import http, _
 from odoo.addons.portal.controllers.portal import CustomerPortal
 from odoo.http import request
+from odoo.osv.expression import OR
 
 
 class CustomerPortal(CustomerPortal):
@@ -21,23 +22,60 @@ class CustomerPortal(CustomerPortal):
         return values
 
     @http.route(['/my/tickets', '/my/tickets/page/<int:page>'], type='http', auth="user", website=True)
-    def my_tickets(self, page=1, date_begin=None, date_end=None, project=None, sortby=None, **kw):
+    def my_tickets(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, search=None, search_in='content', **kw):
+        groupby = 'none'  # kw.get('groupby', 'project') #TODO master fix this
         values = self._prepare_portal_layout_values()
 
-        sortings = {
+        searchbar_sortings = {
             'date': {'label': _('Newest'), 'order': 'create_date desc'},
-            'name': {'label': _('Name'), 'order': 'name'},
+            'name': {'label': _('Title'), 'order': 'name'},
             'stage': {'label': _('Stage'), 'order': 'stage_id'},
-            'update': {'label': _('Last Stage Update'), 'order': 'date_last_stage_update desc'},
+#            'update': {'label': _('Last Stage Update'), 'order': 'date_last_stage_update desc'},
+        }
+        searchbar_filters = {
+            'all': {'label': _('All'), 'domain': []},
+        }
+        searchbar_inputs = {
+            'content': {'input': 'content', 'label': _('Search <span class="nolabel"> (in Content)</span>')},
+            'message': {'input': 'message', 'label': _('Search in Messages')},
+#            'customer': {'input': 'customer', 'label': _('Search in Customer')},
+            'stage': {'input': 'stage', 'label': _('Search in Stages')},
+            'all': {'input': 'all', 'label': _('Search in All')},
+        }
+        searchbar_groupby = {
+            'none': {'input': 'none', 'label': _('None1')},
+            'project': {'input': 'project', 'label': _('Project')},
         }
 
         domain = ([])
-        order = sortings.get(sortby, sortings['date'])['order']
+
+        # default sort by value
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
+        # default filter by value
+        if not filterby:
+            filterby = 'all'
+        domain += searchbar_filters[filterby]['domain']
 
         # archive groups - Default Group By 'create_date'
         archive_groups = self._get_archive_groups('helpdesk_lite.ticket', domain)
         if date_begin and date_end:
             domain += [('create_date', '>', date_begin), ('create_date', '<=', date_end)]
+
+        # search
+        if search and search_in:
+            search_domain = []
+            if search_in in ('content', 'all'):
+                search_domain = OR([search_domain, ['|', ('name', 'ilike', search), ('description', 'ilike', search)]])
+            if search_in in ('customer', 'all'):
+                search_domain = OR([search_domain, [('partner_id', 'ilike', search)]])
+            if search_in in ('message', 'all'):
+                search_domain = OR([search_domain, [('message_ids.body', 'ilike', search)]])
+            if search_in in ('stage', 'all'):
+                search_domain = OR([search_domain, [('stage_id', 'ilike', search)]])
+            domain += search_domain
+
         # pager
         pager = request.website.pager(
             url="/my/tickets",
@@ -52,13 +90,20 @@ class CustomerPortal(CustomerPortal):
         values.update({
             'date': date_begin,
             'date_end': date_end,
-            'sortings': sortings,
             'sortby': sortby,
             'tickets': helpdesk_ticket,
             'page_name': 'ticket',
             'archive_groups': archive_groups,
             'default_url': '/my/tickets',
-            'pager': pager
+            'pager': pager,
+            'searchbar_sortings': searchbar_sortings,
+            'searchbar_groupby': searchbar_groupby,
+            'searchbar_inputs': searchbar_inputs,
+            'search_in': search_in,
+            'sortby': sortby,
+            'groupby': groupby,
+            'searchbar_filters': OrderedDict(sorted(searchbar_filters.items())),
+            'filterby': filterby,
         })
         return request.render("helpdesk_lite.portal_my_tickets", values)
 
