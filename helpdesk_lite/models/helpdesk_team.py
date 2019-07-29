40c05ef49dea1f3d0d3cc2023c63ff57831c7348
KareemAbuzaid <36644028+KareemAbuzaid@users.noreply.github.com>
2019-07-29 11:11:07 +0200

    [FIX] helpdesk_lite: fix import safe_eval

--- helpdesk_lite/models/helpdesk_team.py
+++ helpdesk_lite/models/helpdesk_team.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
-from odoo.tools import safe_eval
+from odoo.tools.safe_eval import safe_eval
 
 
 class SupportTeam(models.Model):
